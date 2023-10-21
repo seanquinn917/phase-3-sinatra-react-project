8c0b10fb37e2c48cd521699caa79c7dab887643f
SeanQuinn <Seanquinn917@gmail.com>
2023-10-21 14:55:19 -0400

    empty seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,5 @@
 
 puts "🌱 Seeding spices..."
 
-Zoo.create(name: "Bronx Zoo", location: "Bronx, New York", population: 4000)
-Zoo.create(name: "Queens", location: "Queens, New York", population: 700)
-Zoo.create(name: "Central Park Zoo", location: "Manhattan, New York", population: 1487)
-Zoo.create(name: "Staten Island Zoo", location: "Staten Island, New York", population: 1400)
-Zoo.create(name: "Propect Park Zoo", location: "Brooklyn New York", population: 864)
-
-# 50.times do 
-#     Animal.create(
-#         name: Faker::Creature::Animal.name,
-#         zoo_id: Zoo.id
-#     )
-# end
-
-
-Animal.create(name: "Bear", )
-
 
 puts "✅ Done seeding!"
