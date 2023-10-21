8e6811480279f259985ea2c8ab5052fab276a4cb
SeanQuinn <Seanquinn917@gmail.com>
2023-10-21 15:24:25 -0400

    create hotel/review migrations and tables

---
+++ db/migrate/20231021185618_create_hotels.rb
@@ -0,0 +1,10 @@
+class CreateHotels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hotels do |t|
+      t.string :name
+      t.string :location
+      t.integer :price
+      t.timestamps
+    end
+  end
+end
